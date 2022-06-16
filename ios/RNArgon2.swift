3e10b6e89a46e9d816dfad2f7e4b48e417225315
Bram ten Cate <btencate@sphereon.com>
2022-06-16 14:13:25 +0200

    MYC-180 iOS salt string to array of UInt8 bytes

--- ios/RNArgon2.swift
+++ ios/RNArgon2.swift
@@ -16,7 +16,7 @@ class RNArgon2: NSObject {
     argon2Context.iterations = configDict["iterations", default: 2 ] as! Int;
     argon2Context.memory = configDict["memory", default: 32 * 1024 ] as! Int;
     argon2Context.parallelism = configDict["parallelism", default: 1 ] as! Int;
-    argon2Context.salt = salt;
+    argon2Context.salt = salt.hexaBytes();
     argon2Context.hashLength = configDict["hashLength", default: 32 ] as! Int;
     argon2Context.mode = getArgon2Mode(mode: configDict["mode", default: "argon2id" ] as! String);
 
